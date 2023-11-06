8428bdf4db8abdc9002033866f6487a0bfa3728a
tumbersa <gleb.kapustin.95@gmail.com>
2023-11-06 12:15:57 +0300

    initial and final commit

---
+++ Sampling Method/ImageProcessor.hpp
@@ -0,0 +1,111 @@
+//
+//  ImageProcessor.cpp
+//  Sampling Method
+//
+//  Created by Глеб Капустин on 06.11.2023.
+//
+#define STB_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "stb_image.h"
+#include "stb_image_write.h"
+
+class ImageProcessor {
+public:
+    ImageProcessor(const char* originalImagePath);
+    ~ImageProcessor();
+
+    void reduceNearestNeighbor(int reducedWidth, int reducedHeight, const char* outputPath);
+    void reduceSampling(int reducedWidth, int reducedHeight, const char* outputPath);
+
+private:
+    int originalWidth;
+    int originalHeight;
+    int originalChannels;
+    unsigned char* originalImage;
+
+    void loadOriginalImage(const char* originalImagePath);
+    void saveReducedImage(const char* outputPath, const unsigned char* reducedImage, int width, int height);
+};
+
+ImageProcessor::ImageProcessor(const char* originalImagePath) {
+    loadOriginalImage(originalImagePath);
+}
+
+ImageProcessor::~ImageProcessor() {
+    stbi_image_free(originalImage);
+}
+
+void ImageProcessor::loadOriginalImage(const char* originalImagePath) {
+    originalImage = stbi_load(originalImagePath, &originalWidth, &originalHeight, &originalChannels, 0);
+    if (!originalImage) {
+        // Handle image loading error
+        // You may want to throw an exception or return an error code here.
+    }
+}
+
+void ImageProcessor::saveReducedImage(const char* outputPath, const unsigned char* reducedImage, int width, int height) {
+    stbi_write_jpg(outputPath, width, height, originalChannels, reducedImage, 100); // Save in JPEG format
+}
+
+void ImageProcessor::reduceNearestNeighbor(int reducedWidth, int reducedHeight, const char* outputPath) {
+    unsigned char* reducedImage = new unsigned char[reducedWidth * reducedHeight * originalChannels];
+
+    for (int y = 0; y < reducedHeight; ++y) {
+        for (int x = 0; x < reducedWidth; ++x) {
+            int orig_x = x * originalWidth / reducedWidth;
+            int orig_y = y * originalHeight / reducedHeight;
+
+            int origIndex = (orig_y * originalWidth + orig_x) * originalChannels;
+            int reducedIndex = (y * reducedWidth + x) * originalChannels;
+
+            for (int c = 0; c < originalChannels; ++c) {
+                reducedImage[reducedIndex + c] = originalImage[origIndex + c];
+            }
+        }
+    }
+
+    saveReducedImage(outputPath, reducedImage, reducedWidth, reducedHeight);
+
+    delete[] reducedImage;
+}
+
+void ImageProcessor::reduceSampling(int reducedWidth, int reducedHeight, const char* outputPath) {
+    unsigned char* reducedImage = new unsigned char[reducedWidth * reducedHeight * originalChannels];
+
+    for (int y = 0; y < reducedHeight; ++y) {
+        for (int x = 0; x < reducedWidth; ++x) {
+            float orig_x = static_cast<float>(x) * (static_cast<float>(originalWidth) / reducedWidth);
+            float orig_y = static_cast<float>(y) * (static_cast<float>(originalHeight) / reducedHeight);
+
+            int orig_x_floor = static_cast<int>(orig_x);
+            int orig_y_floor = static_cast<int>(orig_y);
+            int orig_x_ceil = orig_x_floor + 1;
+            int orig_y_ceil = orig_y_floor + 1;
+
+            float x_weight = orig_x - orig_x_floor;
+            float y_weight = orig_y - orig_y_floor;
+
+            int reducedIndex = (y * reducedWidth + x) * originalChannels;
+
+            for (int c = 0; c < originalChannels; ++c) {
+                float top_left = originalImage[(orig_y_floor * originalWidth + orig_x_floor) * originalChannels + c];
+                float top_right = originalImage[(orig_y_floor * originalWidth + orig_x_ceil) * originalChannels + c];
+                float bottom_left = originalImage[(orig_y_ceil * originalWidth + orig_x_floor) * originalChannels + c];
+                float bottom_right = originalImage[(orig_y_ceil * originalWidth + orig_x_ceil) * originalChannels + c];
+
+                float interpolated_value = top_left * (1 - x_weight) * (1 - y_weight) +
+                                          top_right * x_weight * (1 - y_weight) +
+                                          bottom_left * (1 - x_weight) * y_weight +
+                                          bottom_right * x_weight * y_weight;
+
+                reducedImage[reducedIndex + c] = static_cast<unsigned char>(interpolated_value);
+            }
+        }
+    }
+
+    saveReducedImage(outputPath, reducedImage, reducedWidth, reducedHeight);
+
+    delete[] reducedImage;
+}
--- Sampling Method/main.cpp
+++ Sampling Method/main.cpp
@@ -5,10 +5,20 @@
 //  Created by Глеб Капустин on 06.11.2023.
 //
 
-#include <iostream>
+#include "ImageProcessor.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    const char* originalImagePath = "/Users/gleb_kapustin/Desktop/VSU/Sampling Method/Sampling Method/input.jpg";
+    const char* reducedNNImagePath = "/Users/gleb_kapustin/Desktop/VSU/Sampling Method/Sampling Method/output_nearest_neighbor.jpg";
+    const char* reducedSamplingImagePath = "/Users/gleb_kapustin/Desktop/VSU/Sampling Method/Sampling Method/output_sampling.jpg";
+    
+    ImageProcessor imageProcessor(originalImagePath);
+    
+    int reducedWidth = 350; // Your desired reduced width
+    int reducedHeight = 350; // Your desired reduced height
+    
+    imageProcessor.reduceNearestNeighbor(reducedWidth, reducedHeight, reducedNNImagePath);
+    imageProcessor.reduceSampling(reducedWidth, reducedHeight, reducedSamplingImagePath);
+    
     return 0;
 }
