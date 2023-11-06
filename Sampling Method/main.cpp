8eda529a4940ea1fe34c9105b89070a91a56b2c1
tumbersa <gleb.kapustin.95@gmail.com>
2023-11-06 10:31:03 +0300

    Initial Commit

---
+++ Sampling Method/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sampling Method
+//
+//  Created by Глеб Капустин on 06.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
